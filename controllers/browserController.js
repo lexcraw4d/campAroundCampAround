809c643d8f979fd1eb9c3e25f09b75f42f3d2a4b
Lex Crawford <lexcraw4d@gmail.com>
2022-08-04 23:32:37 -0400

    added functionality to create a campground post

--- controllers/browserController.js
+++ controllers/browserController.js
@@ -1,8 +1,9 @@
 const Campground = require("../models/Campgrounds");
+const { createCampground } = require("./apiController");
 
 const browserController = {
 getCampgrounds: async (req, res) => {
-    let campgrounds;;
+    let campgrounds;
     try{
         campgrounds = await Campground.find({});
     }
@@ -10,7 +11,17 @@ getCampgrounds: async (req, res) => {
         console.log(err);
     }
     res.render("campgrounds", {campgrounds});
-}
-
+},
+createCampground: async (req, res) => {
+    try{ await Campground.create({
+            name: req.body.name,
+            image: req.body.image,
+            description: req.body.description
+        })
+        res.status(200).redirect('/campgrounds');
+    }
+    catch(err){
+        console.log(err);
+    }}
 }
 module.exports = browserController;
--- routes/clientRoutes/pagesRouter.js
+++ routes/clientRoutes/pagesRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getCampgrounds } = require('../../controllers/browserController');
+const { getCampgrounds, createCampground } = require('../../controllers/browserController');
 router
 .get('/', (req, res) => {
   res.render('index');
 })
 .get('/campgrounds', getCampgrounds)
+.post('/createCampground', createCampground)
 
 module.exports = router;
