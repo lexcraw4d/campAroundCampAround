fae943c2f23d0da46c9fde2cefb92a9524c95802
Lex Crawford <lexcraw4d@gmail.com>
2022-09-18 21:36:12 -0400

    added todos list and future enhancement ideas

---
+++ todo.js
@@ -0,0 +1,9 @@
+// TODO: Add sign up page style 
+// TODO: Set up routes for Sign up page
+// TODO: Add an about/contact page including acorn inspiration 
+
+
+//* Future Enhancements
+// TODO: Add Comments and Replies
+// TODO: Maps Look into
+// TODO: Look into Maps (future)
