a90d53877074b1c28edc95dc0c8742cd0ad0a8ae
Lex Crawford <lexcraw4d@gmail.com>
2022-09-25 01:05:23 -0400

    updated todolist

--- todo.js
+++ todo.js
@@ -1,8 +1,9 @@
-// TODO: Add sign up page style 
-// TODO: Set up routes for Sign up page
+// DONE Add sign up page style 
+// DONE Set up routes for Sign up page
 // TODO: Add an about/contact page including acorn inspiration 
 
-
+//? Think of more features to add
+//? Get flash messages working
 //* Future Enhancements
 // TODO: Add Comments and Replies
 // TODO: Maps Look into
