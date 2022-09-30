2e217a3bcb3a689a093e8f931d1c95ba76e8ccce
Lex Crawford <lexcraw4d@gmail.com>
2022-09-30 15:08:10 -0400

    styling css responsive design

--- todo.js
+++ todo.js
@@ -1,12 +1,18 @@
-// DONE Add sign up page style 
-// DONE Set up routes for Sign up page
-// DONE Get flash messages working
+// >DONE Add sign up page style 
+//> DONE Set up routes for Sign up page
+//> DONE Get flash error messages working
+
+
+//* Future features:
+//? Add more styling
+//! Get flash messages working for sign up page success
+//? Maps api?
+//? Campground search
+
+
+
+// TO DO LIST
 // TODO: Add an about/contact page including acorn inspiration 
-
-// do not forget to add the above to the top of your file
-
-//? Think of more features to add
-//* Future Enhancements
 // TODO: Add Comments and Replies
-// TODO: Maps Look into
-// TODO: Look into Maps (future)
+// TODO: Make a readme
+
